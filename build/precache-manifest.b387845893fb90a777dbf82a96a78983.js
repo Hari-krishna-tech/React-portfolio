8e2d2aee0e64725878580080a4ddf525eb9bfd30
Hari-krishna-tech <harikrishna03092@gmail.com>
2020-09-14 20:48:13 -0700

    added build

---
+++ build/precache-manifest.b387845893fb90a777dbf82a96a78983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08f7bd745b8e4035038e7e728d1a800",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42131bf819477074ff30",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42131bf819477074ff30",
+    "url": "/static/js/main.efa7744d.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
